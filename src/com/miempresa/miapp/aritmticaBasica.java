8f9e2c0b631583cfe5ef59a3ee9f336a94c7c752
boris-Ag <adrada0919@gmail.com>
2024-10-25 11:42:00 -0500

    ADD Operaciones

---
+++ src/com/miempresa/miapp/aritmticaBasica.java
@@ -0,0 +1,14 @@
+package com.miempresa.miapp;
+
+public class aritmticaBasica {
+	
+	public static double triangulo(double a,double b) {
+		return (a*b)/2;
+	}
+	
+	public static double cuadrado(double a, double b) {
+		return a*b;
+		
+	}
+
+}
